6b61cfcc29a246fa92f3042beb52745e1a0fd857
Arya Ramandhanu <arya.ramandhanu@gmail.com>
2017-06-21 13:26:44 +0700

    install.sh edit

--- install.sh
+++ install.sh
@@ -2,17 +2,28 @@
 # Laravel Setup for Ubuntu 16.04 LTS
 # 
 
+# root check
+[ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
+
 # update the sources
-sudo apt update
+apt update
 echo "................updated apt repos"
-sleep 2
+sleep 2 	
 
 # install zip & git
-sudo apt -y install zip git
+apt -y install zip git
 echo "................installed git"
 sleep 2
 
 # install nginx
-sudo apt -y install nginx
+apt -y install nginx
 echo "................installed NGINX"
+sleep 2
+
+# install php
+apt-get install python-software-properties -y
+add-apt-repository ppa:ondrej/php
+apt-get update -y
+apt-get install -y php5.6
+echo "................installed PHP"
 sleep 2
