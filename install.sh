55e12ede138c3213e092d9dfbaf88177b97de619
Arya Ramandhanu <arya.ramandhanu@gmail.com>
2017-06-21 14:20:41 +0700

    Add php installer

--- install.sh
+++ install.sh
@@ -7,23 +7,29 @@
 
 # update the sources
 apt update
-echo "................updated apt repos"
+echo "....................................................updated apt repos"
 sleep 2 	
 
 # install zip & git
 apt -y install zip git
-echo "................installed git"
+echo "....................................................installed git"
 sleep 2
-
-# install nginx
-apt -y install nginx
-echo "................installed NGINX"
-sleep 2
+echo ""
 
 # install php
 apt-get install python-software-properties -y
 add-apt-repository ppa:ondrej/php
 apt-get update -y
-apt-get install -y php5.6
-echo "................installed PHP"
+apt-get install -y php5.6 php5.6-curl php5.6-mbstring php5.6-pcre php5.6-pdo php5.6-mysql php5.6-mhash php5.6-mcrypt php5.6-phar
+echo "...................................................installed PHP"
 sleep 2
+echo ""
+
+# install nginx
+apt -y install nginx
+echo "....................................................installed NGINX"
+sleep 2
+echo ""
+
+# nginx config
+echo "Setup nginx........"
